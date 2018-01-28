096b611a19c0b9385e3b059755d93b17414b5939
Luis Villalba <villalba.luis.jjr@gmail.com>
2018-01-27 21:18:55 -0400

    Se ajusto problema, hacia falta cambiar json a array, se uso JSON.pased

--- vistas/js/veralert.js
+++ vistas/js/veralert.js
@@ -4,14 +4,21 @@ var bandera;
 function init(){
     /*USAR ESTO PARA MANDAR A VER ALERTAS POR POST*/
    $.post("controllers/alerta.php?op=verAlertlicencia",function(respuesta){
-       $("#licenciaVencida").html(respuesta);
-       /*RECORREMOS LISTADO POR VENCER*/
-//       for (x=0;x<data.length;x++){
-//           $("#licenciaVencida").append(data.nombre);
-//     }
-       
-//       $("#idcliente").html(respuesta);
-//       $("#idcliente").selectpicker('refresh');
+       /*CONVERTIMOS EL JSON EN UN ARREGLO*/
+       var parsed = JSON.parse(respuesta);
+       for (var i=0;i<parsed.length;i++){
+            $("#licenciaVencida").append("<li>"+parsed[i]['nombre']+"</li>");
+       }
+       /*FORMA DE PASAR UN JSON A UN ARRAGLO CON PUSH DENTRO DEL FORM NO SE USO POR NO HACER FALTA 
+       console.log("valores",parsed);
+       var arr = [];
+       for(var x in parsed){
+       arr.push(parsed[x]);
+       }
+       for (i=0;i<arr.length;i++){
+          console.log("respuesta",arr[i]);
+       }
+       */
    });
 
 }
--- vistas/modulos/veralert.php
+++ vistas/modulos/veralert.php
@@ -73,8 +73,8 @@
                                     <div class="alert alert-danger alert-dismissible">
                                         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
                                         <h4><i class="icon fa fa-ban"></i> Vencido!</h4>Los siguientes choferes tienen su licencia vencida:
-                                        <ul id='licenciaVencida'>
-                                        </ul>
+                                        <ol id='licenciaVencida'>
+                                        </ol>
                                       </div>
                                     <div class="alert alert-warning alert-dismissible">
                                         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
