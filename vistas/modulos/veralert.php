e0475b2680627d773821ffe45ac58dd9fda898b2
Luis Villalba <villalba.luis.jjr@gmail.com>
2018-02-18 17:46:36 -0400

    Se arregla, alerta de licencia y se elimina alerta de cedula vencida por no ser necesaria

--- vistas/modulos/veralert.php
+++ vistas/modulos/veralert.php
@@ -41,33 +41,31 @@
                             </div>
                         </div>
 -->
-                        <div class="col-lg-6 col-md-6 col-xs-12">
+                        <div class="col-lg-4 col-md-6 col-xs-12">
                             <div class="box box-danger">
                                 <div class="box-header with-border"> <i class="fa fa-credit-card"></i>
                                     <h3 class="box-title">Certificados Vencidos</h3> </div>
                                 <div class="box-body">
-                                    <div id="ldanger" class="alert alert-danger alert-dismissible">
+                                    <div id="ldanger2" class="alert alert-danger alert-dismissible">
                                         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
                                         <h4><i class="icon fa fa-ban"></i> Vencido!</h4>Los siguientes choferes tienen su certificado vencido:
                                         <ol id='certificadoVencida'>
                                             
                                         </ol>
                                       </div>
-                                    <div id="lwarning" class="alert alert-warning alert-dismissible">
+                                    <div id="lwarning2" class="alert alert-warning alert-dismissible">
                                         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
                                         <h4><i class="icon fa fa-warning"></i> Advertencia!</h4>Los siguientes choferes tienen su certificado proximo a vencer:
                                         
                                           <ol id='certificadoPorVencer'>
                                           </ol></div>
-                                    <div id="lsuccess" class="alert alert-success alert-dismissible">
+                                    <div id="lsuccess2" class="alert alert-success alert-dismissible">
                                         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
                                         <h4><i class="icon fa fa-check"></i> Exito!</h4> Todas las licencias estan vigentes. </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-lg-6 col-md-6 col-xs-12">
+                        <div class="col-lg-4 col-md-6 col-xs-12">
                             <div class="box box-danger">
                                 <div class="box-header with-border"> <i class="fa fa-credit-card"></i>
                                     <h3 class="box-title">Licencias Vencidas</h3> </div>
@@ -91,7 +89,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col-lg-6 col-md-6 col-xs-12">
+                        <div class="col-lg-4 col-md-6 col-xs-12">
                             <div class="box box-danger">
                                 <div class="box-header with-border"> <i class="fa fa-credit-card"></i>
                                     <h3 class="box-title">Seguros Vencidos</h3> </div>
