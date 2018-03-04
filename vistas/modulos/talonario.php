f158efe865f08fbf0c29267b2b1cf0df44695289
Luis Villalba <villalba.luis.jjr@gmail.com>
2018-03-04 17:54:13 -0400

    Se agrega validacion a modulos en carteleras

--- vistas/modulos/talonario.php
+++ vistas/modulos/talonario.php
@@ -1,120 +1,145 @@
-<?php include_once("vistas/modulos/inc/aside.php"); ?>
-   <body class="hold-transition skin-blue sidebar-mini">
-    <div class="wrapper">
-   <div class="content-wrapper">
-    <section class="content-header">
+<?php
+  if(!$_SESSION['validarTSM']){
+      header("location:inicio");
+      exit();
+  } else {
+      if($_SESSION['carteleras']==1){
+        include_once("vistas/modulos/inc/aside.php");
+?>
+<!-- INICIO DEL M.TALONARIO -->
+<body class="hold-transition skin-blue sidebar-mini">
+  <div class="wrapper">
+    <div class="content-wrapper">
+      <section class="content-header">
         <h1>
-        Control
-        <small>Talonarios entregados</small>
-      </h1>
+          Control
+          <small>Talonarios entregados</small>
+        </h1>
         <ol class="breadcrumb">
-            <li><a href="#"><i class="fa fa-dashboard"></i> Otros</a></li>
-            <li class="active">Talonarios</li>
+          <li>
+            <a href="#">
+              <i class="fa fa-dashboard"></i> Otros</a>
+          </li>
+          <li class="active">Talonarios</li>
         </ol>
         <section class="content">
-            <div class="row">
-              <div class="col-md-12">
-                  <div class="box">
-                    <div class="box-header with-border">
-                          <h1 class="box-title">Talonarios</h1>
-                          <button class="pull-right btn btn-success" id="btnagregar" onclick="mostrarform(true)"><i class="fa fa-plus-circle"></i> Agregar</button>
-                        <div class="box-tools pull-right">
-                        </div>
-                    </div>
-                    <!-- /.box-header -->
-                    <!-- centro -->
-                    <div class="panel-body table-responsive" id="listadoregistros">
-                      <table id="tbllistado" class="table table-striped table-bordered table-condensed table-hover">
-                        <thead>
-                          <th>Opciones</th>
-                          <th>Chofer</th>
-                          <th>Desde</th>
-                          <th>Hasta</th>
-                          <th>Fecha</th>
-                        </thead>
-                        <tbody>
+          <div class="row">
+            <div class="col-md-12">
+              <div class="box">
+                <div class="box-header with-border">
+                  <h1 class="box-title">Talonarios</h1>
+                  <button class="pull-right btn btn-success" id="btnagregar" onclick="mostrarform(true)">
+                    <i class="fa fa-plus-circle"></i> Agregar</button>
+                  <div class="box-tools pull-right">
+                  </div>
+                </div>
+                <!-- /.box-header -->
+                <!-- centro -->
+                <div class="panel-body table-responsive" id="listadoregistros">
+                  <table id="tbllistado" class="table table-striped table-bordered table-condensed table-hover">
+                    <thead>
+                      <th>Opciones</th>
+                      <th>Chofer</th>
+                      <th>Desde</th>
+                      <th>Hasta</th>
+                      <th>Fecha</th>
+                    </thead>
+                    <tbody>
 
-                        </tbody>
-                        <tfoot>
-                        <th>Opciones</th>
-                        <th>Chofer</th>
-                        <th>Desde</th>
-                        <th>Hasta</th>
-                        <th>Fecha</th>
-                        </tfoot>
-                      </table>
+                    </tbody>
+                    <tfoot>
+                      <th>Opciones</th>
+                      <th>Chofer</th>
+                      <th>Desde</th>
+                      <th>Hasta</th>
+                      <th>Fecha</th>
+                    </tfoot>
+                  </table>
+                </div>
+                <div class="panel-body" id="formularioregistros">
+                  <form name="formulario" id="formulario" method="POST">
+                    <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                      <input type="hidden" name="idtalonario" id="idtalonario">
+                      <label>Chofer*:</label>
+                      <!-- INCLUIMOS LA CLASE SELECTPICKER -->
+                      <select id="idchofer" class="form-control selectpicker" data-live-search="true" name="idchofer" required>
+                      </select>
                     </div>
-                    <div class="panel-body" id="formularioregistros">
-                      <form name="formulario" id="formulario" method="POST">
-                         <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                        <input type="hidden" name="idtalonario" id="idtalonario">
-                         <label>Chofer*:</label>   <!-- INCLUIMOS LA CLASE SELECTPICKER -->
-                          <select id="idchofer" class="form-control selectpicker" data-live-search="true" name="idchofer" required>
-                          </select>
-                        </div>
-                        <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                          <label>Fecha de entrega*:</label>
-                          <input type="date" class="form-control" name="fecha" id="fecha" required>
-                        </div>
-                          <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                          <label>Desde*:</label>
-                          <input type="number" class="form-control" name="desde" id="desde" min="0" required>
-                        </div>
-                          <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                          <label>Hasta*:</label>
-                          <input type="number" class="form-control" name="hasta" id="hasta" min="0" required>
-                        </div>
-                        <div class="form-group col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                          <button class="btn btn-primary" type="submit" id="btnGuardar"><i class="fa fa-save"></i> Guardar</button>
-                          <button class="btn btn-danger" type="button" onclick="cancelarform()"><i class="fa fa-arrow-circle-left"></i> Cancelar</button>
-                            <button type="button" class="btn btn-info" data-toggle="modal" data-target="#modal-primary">
-                            Ayuda
-                            </button>
-                        </div>
-                      </form>
+                    <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                      <label>Fecha de entrega*:</label>
+                      <input type="date" class="form-control" name="fecha" id="fecha" required>
                     </div>
-                    <!--Fin centro -->
-                  </div><!-- /.box -->
-              </div><!-- /.col -->
-          </div><!-- /.row -->
-      </section>
+                    <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                      <label>Desde*:</label>
+                      <input type="number" class="form-control" name="desde" id="desde" min="0" required>
+                    </div>
+                    <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                      <label>Hasta*:</label>
+                      <input type="number" class="form-control" name="hasta" id="hasta" min="0" required>
+                    </div>
+                    <div class="form-group col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                      <button class="btn btn-primary" type="submit" id="btnGuardar">
+                        <i class="fa fa-save"></i> Guardar</button>
+                      <button class="btn btn-danger" type="button" onclick="cancelarform()">
+                        <i class="fa fa-arrow-circle-left"></i> Cancelar</button>
+                      <button type="button" class="btn btn-info" data-toggle="modal" data-target="#modal-primary">
+                        Ayuda
+                      </button>
+                    </div>
+                  </form>
+                </div>
+                <!--Fin centro -->
+              </div>
+              <!-- /.box -->
+            </div>
+            <!-- /.col -->
+          </div>
+          <!-- /.row -->
+        </section>
         <div class="modal modal-primary fade" id="modal-primary">
-                      <div class="modal-dialog">
-                        <div class="modal-content">
-                          <div class="modal-header">
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Cerrar">
-                              <span aria-hidden="true">×</span></button>
-                            <h4 class="modal-title">Modulo Licencias</h4>
-                          </div>
-                          <div class="modal-body">
-                             <div class="box-body">
-                                          <dl class="dl-horizontal">
-                                            <dt>Grado</dt>
-                                            <dd style="text-align:justify">Seleccione el grado de la licencia.</dd>
-                                            <dt>Vence</dt>
-                                            <dd style="text-align:justify">Ingrese la fecha de vencimiento de la licencia.</dd>
-                                          </dl>
-                                    </div>
-                          </div>
-                          <div class="modal-footer">
-                            <button type="button" class="btn btn-outline pull-left" data-dismiss="modal">Cerrar</button>
-                          </div>
-                        </div>
-                        <!-- /.modal-content -->
-                      </div>
-                      <!-- /.modal-dialog -->
-                    </div>
-    </section>
-    <!-- Main content -->
-    
-    <!-- /.content -->
-    <?php include_once("vistas/modulos/inc/footer.php"); ?>
-        <script src="vistas/plugins/datatables/jquery.dataTables.min.js"></script>    
-    <script src="vistas/plugins/datatables/dataTables.buttons.min.js"></script>
-    <script src="vistas/plugins/datatables/buttons.html5.min.js"></script>
-    <script src="vistas/plugins/datatables/buttons.colVis.min.js"></script>
-    <script src="vistas/plugins/datatables/jszip.min.js"></script>
-    <script src="vistas/plugins/datatables/pdfmake.min.js"></script>
-    <script src="vistas/plugins/datatables/vfs_fonts.js"></script> 
-    <script type="text/javascript" src="vistas/js/talonario.js"></script>
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Cerrar">
+                  <span aria-hidden="true">×</span>
+                </button>
+                <h4 class="modal-title">Modulo Licencias</h4>
+              </div>
+              <div class="modal-body">
+                <div class="box-body">
+                  <dl class="dl-horizontal">
+                    <dt>Grado</dt>
+                    <dd style="text-align:justify">Seleccione el grado de la licencia.</dd>
+                    <dt>Vence</dt>
+                    <dd style="text-align:justify">Ingrese la fecha de vencimiento de la licencia.</dd>
+                  </dl>
+                </div>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-outline pull-left" data-dismiss="modal">Cerrar</button>
+              </div>
+            </div>
+            <!-- /.modal-content -->
+          </div>
+          <!-- /.modal-dialog -->
+        </div>
+      </section>
+<?php include_once("vistas/modulos/inc/footer.php"); ?>
+<script src="vistas/plugins/datatables/jquery.dataTables.min.js"></script>    
+<script src="vistas/plugins/datatables/dataTables.buttons.min.js"></script>
+<script src="vistas/plugins/datatables/buttons.html5.min.js"></script>
+<script src="vistas/plugins/datatables/buttons.colVis.min.js"></script>
+<script src="vistas/plugins/datatables/jszip.min.js"></script>
+<script src="vistas/plugins/datatables/pdfmake.min.js"></script>
+<script src="vistas/plugins/datatables/vfs_fonts.js"></script> 
+<script type="text/javascript" src="vistas/js/talonario.js"></script>
 </div>
+<!-- FIN DEL M.TAlONARIO -->
+<?php
+    } else {
+      header("location:escritorio");
+      ob_end_flush();
+      exit();
+    }
+  }
+?>
--- vistas/modulos/tarifas.php
+++ vistas/modulos/tarifas.php
@@ -1,122 +1,145 @@
-<?php include_once("vistas/modulos/inc/aside.php"); ?>
-   <body class="hold-transition skin-blue sidebar-mini">
-    <div class="wrapper">
-   <div class="content-wrapper">
-    <section class="content-header">
+<?php
+  if(!$_SESSION['validarTSM']){
+      header("location:inicio");
+      exit();
+  } else {
+      if($_SESSION['carteleras']==1){
+        include_once("vistas/modulos/inc/aside.php");
+?>
+<!-- INICIO DEL M.TARIFAS -->
+<body class="hold-transition skin-blue sidebar-mini">
+  <div class="wrapper">
+    <div class="content-wrapper">
+      <section class="content-header">
         <h1>
-        Tarifas
-        <small>TSM / CARIBBEAN</small>
-      </h1>
+          Tarifas
+          <small>TSM / CARIBBEAN</small>
+        </h1>
         <ol class="breadcrumb">
-            <li><a href="#"><i class="fa fa-dashboard"></i> Otros</a></li>
-            <li class="active">Tarifas</li>
+          <li>
+            <a href="#">
+              <i class="fa fa-dashboard"></i> Otros</a>
+          </li>
+          <li class="active">Tarifas</li>
         </ol>
         <section class="content">
-            <div class="row">
-              <div class="col-md-12">
-                  <div class="box">
-                    <div class="box-header with-border">
-                          <h1 class="box-title">Tarifas TSM - CARIBBEAN</h1>
-                          <button class="pull-right btn btn-success" id="btnagregar" onclick="mostrarform(true)"><i class="fa fa-plus-circle"></i> Agregar</button>
-                        <div class="box-tools pull-right">
-                        </div>
+          <div class="row">
+            <div class="col-md-12">
+              <div class="box">
+                <div class="box-header with-border">
+                  <h1 class="box-title">Tarifas TSM - CARIBBEAN</h1>
+                  <button class="pull-right btn btn-success" id="btnagregar" onclick="mostrarform(true)">
+                    <i class="fa fa-plus-circle"></i> Agregar</button>
+                  <div class="box-tools pull-right">
+                  </div>
+                </div>
+                <!-- /.box-header -->
+                <!-- centro -->
+                <div class="panel-body table-responsive" id="listadoregistros">
+                  <table id="tbllistado" class="table table-striped table-bordered table-condensed table-hover">
+                    <thead>
+                      <th>Opciones</th>
+                      <th>Ruta</th>
+                      <th>T. Chofer</th>
+                      <th>T. TSM</th>
+                      <th>T. CARIBBEAN</th>
+                      </tfoot>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                    <tfoot>
+                      <th>Opciones</th>
+                      <th>Ruta</th>
+                      <th>T. Chofer</th>
+                      <th>T. TSM</th>
+                      <th>T. CARIBBEAN</th>
+                    </tfoot>
+                  </table>
+                </div>
+                <div class="panel-body" id="formularioregistros">
+                  <form name="formulario" id="formulario" method="POST">
+                    <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                      <label>Ruta *:</label>
+                      <input type="hidden" name="idtarifa" id="idtarifa">
+                      <input type="text" class="form-control" name="nombre" id="nombre" placeholder="Ruta" required>
                     </div>
-                    <!-- /.box-header -->
-                    <!-- centro -->
-                    <div class="panel-body table-responsive" id="listadoregistros">
-                      <table id="tbllistado" class="table table-striped table-bordered table-condensed table-hover">
-                        <thead>
-                        <th>Opciones</th>
-                        <th>Ruta</th>
-                        <th>T. Chofer</th>
-                        <th>T. TSM</th>
-                        <th>T. CARIBBEAN</th>
-                        </tfoot>
-                        </thead>
-                        <tbody>
-
-                        </tbody>
-                        <tfoot>
-                        <th>Opciones</th>
-                        <th>Ruta</th>
-                        <th>T. Chofer</th>
-                        <th>T. TSM</th>
-                        <th>T. CARIBBEAN</th>
-                        </tfoot>
-                      </table>
+                    <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                      <label>Monto Chofer *:</label>
+                      <input type="number" class="form-control" name="montotsmp" id="montotsmp" required>
                     </div>
-                    <div class="panel-body" id="formularioregistros">
-                      <form name="formulario" id="formulario" method="POST">
-                         <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                          <label>Ruta *:</label>
-                          <input type="hidden" name="idtarifa" id="idtarifa">
-                           <input type="text" class="form-control" name="nombre" id="nombre" placeholder="Ruta" required>
-                        </div>
-                        <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                          <label>Monto Chofer *:</label>
-                          <input type="number" class="form-control" name="montotsmp" id="montotsmp" required>
-                        </div>
-                        <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                         <label>Monto TSM *:</label>
-                          <input type="number" class="form-control" name="montotsmc" id="montotsmc" required>
-                        </div>
-                        <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                         <label>Monto CARIBBEAN *:</label>
-                          <input type="number" class="form-control" name="montocaribec" id="montocaribec" required>
-                        </div>
-                        <div class="form-group col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                          <button class="btn btn-primary" type="submit" id="btnGuardar"><i class="fa fa-save"></i> Guardar</button>
-                          <button class="btn btn-danger" type="button" onclick="cancelarform()"><i class="fa fa-arrow-circle-left"></i> Cancelar</button>
-                            <button type="button" class="btn btn-info" data-toggle="modal" data-target="#modal-primary">
-                            Ayuda
-                            </button>
-                        </div>
-                      </form>
+                    <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                      <label>Monto TSM *:</label>
+                      <input type="number" class="form-control" name="montotsmc" id="montotsmc" required>
                     </div>
-                    <!--Fin centro -->
-                  </div><!-- /.box -->
-              </div><!-- /.col -->
-          </div><!-- /.row -->
-      </section>
+                    <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                      <label>Monto CARIBBEAN *:</label>
+                      <input type="number" class="form-control" name="montocaribec" id="montocaribec" required>
+                    </div>
+                    <div class="form-group col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                      <button class="btn btn-primary" type="submit" id="btnGuardar">
+                        <i class="fa fa-save"></i> Guardar</button>
+                      <button class="btn btn-danger" type="button" onclick="cancelarform()">
+                        <i class="fa fa-arrow-circle-left"></i> Cancelar</button>
+                      <button type="button" class="btn btn-info" data-toggle="modal" data-target="#modal-primary">
+                        Ayuda
+                      </button>
+                    </div>
+                  </form>
+                </div>
+                <!--Fin centro -->
+              </div>
+              <!-- /.box -->
+            </div>
+            <!-- /.col -->
+          </div>
+          <!-- /.row -->
+        </section>
         <div class="modal modal-primary fade" id="modal-primary">
-                      <div class="modal-dialog">
-                        <div class="modal-content">
-                          <div class="modal-header">
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Cerrar">
-                              <span aria-hidden="true">×</span></button>
-                            <h4 class="modal-title">Modulo Seguros</h4>
-                          </div>
-                          <div class="modal-body">
-                             <div class="box-body">
-                                          <dl class="dl-horizontal">
-                                            <dt>Numero</dt>
-                                            <dd style="text-align:justify">Ingrese el numero de seguro del vehiculo.</dd>
-                                            <dt>Vence</dt>
-                                            <dd style="text-align:justify">Indique la fecha de vencimiento del seguro del vehiculo.</dd>
-                                            <dt>Tipo</dt>
-                                            <dd style="text-align:justify">Seleccione el tipo de seguro que posee el vehiculo.</dd>
-                                          </dl>
-                                    </div>
-                          </div>
-                          <div class="modal-footer">
-                            <button type="button" class="btn btn-outline pull-left" data-dismiss="modal">Cerrar</button>
-                          </div>
-                        </div>
-                        <!-- /.modal-content -->
-                      </div>
-                      <!-- /.modal-dialog -->
-                    </div>
-    </section>
-    <!-- Main content -->
-
-    <!-- /.content -->
-    <?php include_once("vistas/modulos/inc/footer.php"); ?>
-    <script src="vistas/plugins/datatables/jquery.dataTables.min.js"></script>    
-    <script src="vistas/plugins/datatables/dataTables.buttons.min.js"></script>
-    <script src="vistas/plugins/datatables/buttons.html5.min.js"></script>
-    <script src="vistas/plugins/datatables/buttons.colVis.min.js"></script>
-    <script src="vistas/plugins/datatables/jszip.min.js"></script>
-    <script src="vistas/plugins/datatables/pdfmake.min.js"></script>
-    <script src="vistas/plugins/datatables/vfs_fonts.js"></script> 
-    <script type="text/javascript" src="vistas/js/tarifas.js"></script>
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Cerrar">
+                  <span aria-hidden="true">×</span>
+                </button>
+                <h4 class="modal-title">Modulo Seguros</h4>
+              </div>
+              <div class="modal-body">
+                <div class="box-body">
+                  <dl class="dl-horizontal">
+                    <dt>Numero</dt>
+                    <dd style="text-align:justify">Ingrese el numero de seguro del vehiculo.</dd>
+                    <dt>Vence</dt>
+                    <dd style="text-align:justify">Indique la fecha de vencimiento del seguro del vehiculo.</dd>
+                    <dt>Tipo</dt>
+                    <dd style="text-align:justify">Seleccione el tipo de seguro que posee el vehiculo.</dd>
+                  </dl>
+                </div>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-outline pull-left" data-dismiss="modal">Cerrar</button>
+              </div>
+            </div>
+            <!-- /.modal-content -->
+          </div>
+          <!-- /.modal-dialog -->
+        </div>
+      </section>
+<?php include_once("vistas/modulos/inc/footer.php"); ?>
+<script src="vistas/plugins/datatables/jquery.dataTables.min.js"></script>    
+<script src="vistas/plugins/datatables/dataTables.buttons.min.js"></script>
+<script src="vistas/plugins/datatables/buttons.html5.min.js"></script>
+<script src="vistas/plugins/datatables/buttons.colVis.min.js"></script>
+<script src="vistas/plugins/datatables/jszip.min.js"></script>
+<script src="vistas/plugins/datatables/pdfmake.min.js"></script>
+<script src="vistas/plugins/datatables/vfs_fonts.js"></script> 
+<script type="text/javascript" src="vistas/js/tarifas.js"></script>
 </div>
+<!-- FIN DEL M.TARIFAS -->
+<?php
+    } else {
+      header("location:escritorio");
+      ob_end_flush();
+      exit();
+    }
+  }
+?>
