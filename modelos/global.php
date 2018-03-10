97dd8e7344a29285d79956f2f08da6b390a05ebf
Luis Villalba <villalba.luis.jjr@gmail.com>
2018-03-09 23:32:08 -0400

    Se agregan cambios para subir al servidor

--- modelos/global.php
+++ modelos/global.php
@@ -11,3 +11,17 @@ define("DB_PASSWORD","");
 define("DB_ENCODE","uft8");
 /*Nombre proyecto*/
 define("PRO_NOMBRE","sigtsm");
+
+/*CONEXION DEL SERVIDOR*/
+
+// define("DB_HOST","gator2016.hostgator.com");
+// /*nombre*/
+// define("DB_NAME","otjlvill_sigtsm");
+// /*user*/
+// define("DB_USERNAME","otjlvill_tsmplc2");
+// /*pw*/
+// define("DB_PASSWORD","sistemas.plc.2018");
+// /*Codificacion*/
+// define("DB_ENCODE","uft8");
+// /*Nombre proyecto*/
+// define("PRO_NOMBRE","sigtsm");
